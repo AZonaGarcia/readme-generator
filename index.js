be77fa5fe7957e92cead12299a0d6cc057e796b8
Bonus Mom <AZonaGarcia@aol.com>
2020-08-26 00:46:51 -0400

    created prompts

--- index.js
+++ index.js
@@ -2,9 +2,42 @@ var inquirer = require("inquirer");
 var fs = require("fs")
 
 
-inquirer.prompt ([
-    {},
-    {},
+inquirer.prompt([
+    {
+        type: "input",
+        message: "What is the title of the project",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "What is the description of the project?",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "What is the installation of the application?",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "What is the usage?",
+        name: "usage"
+    },
+    {
+        type: "list",
+        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
+        name: "license"
+    },
+    {
+        type: "input",
+        message: "Credits?",
+        name: "credits"
+    },
+    {
+        type: "input",
+        message: "What is the usage?",
+        name: "usage"
+    }
 ]).then(answers => {
     console.log(answers)
 
